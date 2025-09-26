76f6b9e7667e95723ac0bbfe10800c3015550e03
dancanodhiambo@gmail.com <dancan_odhiambo@Dancan.>
2025-09-26 08:06:19 +0100

    Final commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    """Write content to a .txt file. (overwrites if file exists)"""
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    """Appends content to a .txt file. (creates file if it doesn't exist)"""
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    """Reads content from a .txt file and returns it as a string."""
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read()
